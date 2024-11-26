8a6217336f045993737ad199edd188cb5d2079c6
Hea092024 <Hea092024@gmail.com>
2024-11-26 10:43:25 +0100

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const ghibliUrl = "https://ghibliapi.vercel.app/films/";
+const carManufactureUrl =
+  "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
+
 const httpStatus = "https://httpstat.us/404";
 
 async function fetchData(url) {
@@ -6,17 +8,17 @@ async function fetchData(url) {
   const data = await res.json();
   return data;
 }
-const test = await fetchData(ghibliUrl);
-console.log(fetchData(ghibliUrl));
+const test = await fetchData(carManufactureUrl);
+console.log(fetchData(carManufactureUrl));
 console.log(test);
 
-function useThen(url) {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => console.log("error", error));
-}
-console.log(await useThen(httpStatus));
+// function useThen(url) {
+//   return fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data)
+//     .catch((error) => console.log("error", error));
+// }
+// console.log(await useThen(httpStatus));
 
 // const response = await fetch(ghibliUrl);
 // const data = await response.json();
@@ -29,3 +31,29 @@ console.log(await useThen(httpStatus));
 // // };
 // const test = useAwait(ghibliUrl);
 // console.log(useAwait(test));
+
+// const newApiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+// async function fetchData(url) {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   return data;
+// }
+
+// const newApiData = await fetchData(newApiUrl);
+// console.log(newApiData);
+
+// function useThen(url) {
+//   return fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data)
+//     .catch((error) => console.log("Error with .then():", error));
+// }
+
+// useThen(newApiUrl).then((data) => {
+//   console.log("Data from new API using .then():", data);
+// });
+
+// const response = await fetch(newApiUrl);
+// const data = await response.json();
+// console.log("Data from new API using await:", data);
